41a5cde686644865b2e401935d2750f0a9c737b9
onemedia <onemediaasia@duck.com>
2026-08-24 21:50:32 +0800

    d

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -168,7 +168,7 @@ function Index() {
               alt="Diving In Asia logo"
               className="h-12 w-auto object-contain"
             />
-            <span className="font-display text-lg font-semibold leading-tight text-white/70">
+            <span className="font-display text-lg font-semibold leading-tight text-white/55">
               Diving In Asia
             </span>
           </a>
